e6719654d7c645ba97dfcc9acc68aaf59a53c1a1
Marcin Kass <marcin.kass10@gmail.com>
2017-01-22 20:30:58 +0100

    Add project files.

---
+++ Source/Gigasecond.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GigaSecond
+{
+    public class Gigasecond
+    {
+        private static TimeSpan GigaSecond = new TimeSpan(0,0, 1000000000);
+        public static DateTime Date(DateTime dateNow)
+        {
+            return dateNow.Add(GigaSecond);
+        }
+
+    }
+}
